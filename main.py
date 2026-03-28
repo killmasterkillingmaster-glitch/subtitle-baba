2655b54d58b2d39f88e17e4b258e204fd7ec63ba
killmasterkillingmaster-glitch <killmasterkillingmaster@gmail.com>
2026-03-28 21:48:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from aiohttp import web
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-PORT = int(os.environ.get("PORT", "8080"))
+PORT = int(os.environ.get("PORT", "10000"))
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "5344078567"))
 
