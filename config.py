904ae081ec21a1c95aededd4fc8d1c108ad94504
killmasterkillingmaster-glitch <killmasterkillingmaster@gmail.com>
2026-03-31 22:22:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,13 @@
 import os
 
-# ------------------ TELEGRAM BOT ------------------
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash_here")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
+API_ID = int(os.environ.get("API_ID", "123456"))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "5351848105"))
-ALLOWED_USERS = [5344078567]
+OWNER_ID = int(os.environ.get("OWNER_ID", "5351848105"))
+ALLOWED_USERS = [5344078567, OWNER_ID]
 ALLOWED_GROUPS = [-1003899919015]
 
-# ------------------ DATABASE ------------------
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://YOUR_USER:YOUR_PASS@cluster.mongodb.net/?retryWrites=true&w=majority")
-STORAGE_CHANNEL_ID = -1003096528862
-
-# ------------------ BOT CONFIG ------------------
-PORT = int(os.getenv("PORT", 10000))
-SHORTNER_ACCOUNTS = []
+MONGO_URI = os.environ.get("MONGO_URI", "")
+STORAGE_CHANNEL_ID = int(os.environ.get("STORAGE_CHANNEL_ID", "-1003096528862"))
+PORT = int(os.environ.get("PORT", "10000"))
