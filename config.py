67de341b5ece72e1e2951a87364c0d93c341efb3
killmasterkillingmaster-glitch <killmasterkillingmaster@gmail.com>
2026-04-01 16:59:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-API_ID = 26508821 # Apna API ID dale (my.telegram.org)
-API_HASH = "your_api_hash_here" # Apna API Hash dale
-BOT_TOKEN = "your_bot_token_here"
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
 
 OWNER_ID = 5351848105
 ALLOWED_USERS = [5351848105, 5344078567]
 ALLOWED_GROUP = -1003899919015
 STORAGE_CHANNEL = -1003096528862
 
-MONGO_URI = "mongodb+srv://aasifhusenaasifkhan_db_user:64CtKuQjWL0EzYMO@botcluster.v4land1.mongodb.net/?retryWrites=true&w=majority"
-DB_NAME = "AnimePostBot"
-
-PORT = 10000
+STORAGE_CHANNEL = -1003096528862   # ASI anime (tu ne full admin diya hai)
+PORT = int(os.getenv("PORT", 10000))
