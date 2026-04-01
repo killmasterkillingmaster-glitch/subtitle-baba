4c22ca3a012aa49277cf0464dbcbd4bc06fed4c3
killmasterkillingmaster-glitch <killmasterkillingmaster@gmail.com>
2026-04-01 17:22:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
-
+# Render environment variables se aayega
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 
-OWNER_ID = 5351848105
-ALLOWED_USERS = [5351848105, 5344078567]
-ALLOWED_GROUP = -1003899919015
-STORAGE_CHANNEL = -1003096528862
-
-STORAGE_CHANNEL = -1003096528862   # ASI anime (tu ne full admin diya hai)
 PORT = int(os.getenv("PORT", 10000))
+
+# Hardcoded details as requested
+OWNER_ID = 5351848105
+ALLOWED_USERS = [5344078567, 5351848105]
+ALLOWED_GROUP = -1003899919015
+STORAGE_CHANNEL = -1003096528862  # ASI anime
