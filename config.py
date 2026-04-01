4248ba3d10a46a2679e1366b71cd7b437ff23b1d
killmasterkillingmaster-glitch <killmasterkillingmaster@gmail.com>
2026-04-01 14:50:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+API_ID = 26508821 # Apna API ID dale (my.telegram.org)
+API_HASH = "your_api_hash_here" # Apna API Hash dale
+BOT_TOKEN = "your_bot_token_here"
+
+OWNER_ID = 5351848105
+ALLOWED_USERS = [5351848105, 5344078567]
+ALLOWED_GROUP = -1003899919015
+STORAGE_CHANNEL = -1003096528862
+
+MONGO_URI = "mongodb+srv://aasifhusenaasifkhan_db_user:64CtKuQjWL0EzYMO@botcluster.v4land1.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "AnimePostBot"
+
+PORT = 10000
