1b7c77a8798c77005223140731e87c9c3c30cf76
killmasterkillingmaster-glitch <killmasterkillingmaster@gmail.com>
2026-04-05 22:22:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ from pyrogram.errors import MessageNotModified, MessageIdInvalid
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-PORT = 10000      # ye change mat karna
-
+PORT = int(os.getenv("PORT", 10000))  # ✅ ye line honi chahiye
 OWNER_ID = 5344078567
 ALLOWED_USERS = [5344078567]
 ALLOWED_GROUPS = [-1003810374456]
